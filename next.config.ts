eedc65ac3ce62f69dc03a8a78e7f890fc7144d65
hh <2141509008>
2026-07-28 16:56:21 +0800

    feat: 实现 BBF 架构和熔断策略

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const nextConfig: NextConfig = {
   reactCompiler: true,
   serverExternalPackages: ['some-package'],
   productionBrowserSourceMaps: false,
+  telemetry: false,
 };
 
 export default nextConfig;
